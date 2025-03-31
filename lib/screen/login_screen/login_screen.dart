f3e810058cd08e424498204cb04c7222bd028ec3
HanND <HanND.B21CN324@stu.ptit.edu.vn>
2025-03-31 23:37:07 +0700

    init login

--- lib/screen/login_screen/login_screen.dart
+++ lib/screen/login_screen/login_screen.dart
@@ -5,6 +5,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../../core/widget/base_text_field.dart';
+
 @RoutePage()
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -37,13 +39,88 @@ class _LoginScreenState extends State<LoginScreen> {
                   style: TextStyleUtils.bold(
                     fontSize: 48,
                     color: ColorUtils.backgroundColor,
-                  )
+                  ),
                 ),
               ],
             ),
           ),
+          _loginWidget()
         ],
       ),
+    );
+  }
+
+  Widget _loginWidget() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: ColorUtils.backgroundColor,
+            borderRadius: BorderRadius.circular(10),
+          ),
+          margin: const EdgeInsets.all(16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              const SizedBox(height: 16),
+              Text(
+                'Login',
+                style: TextStyleUtils.bold(
+                  fontSize: 24,
+                  color: ColorUtils.textColor,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: BaseTextField(
+                  controller: TextEditingController(),
+                  hintText: 'Username',
+                  prefixIcon: const Icon(Icons.email),
+                ),
+              ),
+              const SizedBox(height: 16),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                child: BaseTextField(
+                  controller: TextEditingController(),
+                  hintText: 'Password',
+                  prefixIcon: const Icon(Icons.lock),
+                  obscureText: true,
+                ),
+              ),
+              const SizedBox(height: 16),
+              Row(
+                children: [
+                  Expanded(child: Container()),
+                  Expanded(
+                    child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          padding: const EdgeInsets.all(4),
+                          decoration: BoxDecoration(
+                              color: ColorUtils.primaryColor,
+                              borderRadius: const BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                bottomRight: Radius.circular(10),
+                              )),
+                          child: Center(
+                            child: Text(
+                              'Login',
+                              style: TextStyleUtils.bold(
+                                color: ColorUtils.backgroundColor,
+                              ),
+                            ),
+                          ),
+                        )),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
