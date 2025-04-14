9cec74f2afa70084103fa6f2de20de0568f84554
TrungLQ <secrettropro@gmail.com>
2025-04-14 23:03:11 +0700

    init language mode

---
+++ lib/core/language/inherited_language_widget.dart
@@ -0,0 +1,36 @@
+import 'package:app_chat/core/language/data/base_language.dart';
+import 'package:app_chat/core/language/data/en_language.dart';
+import 'package:app_chat/core/language/data/vi_language.dart';
+import 'package:flutter/material.dart';
+
+enum LanguageModeEnum { en, vi }
+
+class InheritedLanguageWidget extends InheritedWidget {
+  final LanguageModeEnum? languageModeEnum;
+
+  const InheritedLanguageWidget({
+    required super.child,
+    this.languageModeEnum,
+    super.key,
+  });
+
+  @override
+  bool updateShouldNotify(InheritedLanguageWidget oldWidget) {
+    return languageModeEnum != oldWidget.languageModeEnum;
+  }
+
+  static InheritedLanguageWidget? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<InheritedLanguageWidget>();
+  }
+
+  BaseLanguage getThemeData() {
+    switch (languageModeEnum) {
+      case LanguageModeEnum.en:
+        return EnLanguage();
+      case LanguageModeEnum.vi:
+        return ViLanguage();
+      default:
+        return EnLanguage();
+    }
+  }
+}
